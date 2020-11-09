42fc6cd39152737597c2bd6d52e0583aac310064
Jackie Llido <73676788+galawangrianne@users.noreply.github.com>
2020-11-09 14:23:16 +0800

    Initial Commit

---
+++ Assets/Scripts/ObjectRotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotator : MonoBehaviour
+{
+    public float speed;
+
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0); 
+         // transform.Rotate function rotates an object around any axis
+         // We will rorate the object around y-axis at "speed" degree per second
+    }
+}
